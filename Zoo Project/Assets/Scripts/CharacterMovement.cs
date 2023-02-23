881ea074f59a2ab52b2e4e9c3bea862dad8dc8cb
Vilius <alaunisvilius@gmail.com>
2023-02-23 12:16:59 +0100

    Rhino Added and Movement

--- Zoo Project/Assets/Scripts/CharacterMovement.cs
+++ Zoo Project/Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    private Vector3 position;
+    private CharacterController controller;
+    public int speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement_v = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        Debug.Log(movement_v);
+
+        controller.Move(movement_v);
     }
 }

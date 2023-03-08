27c4782eefb5150b4b6205544048a2a7db3ed98c
Vilius <alaunisvilius@gmail.com>
2023-03-08 15:13:14 +0100

    Started on movement

--- Zoo Project/Assets/Scripts/CharacterMovement.cs
+++ Zoo Project/Assets/Scripts/CharacterMovement.cs
@@ -5,14 +5,42 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation= true;
     }
 
     void Update()
     {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
+        rb.rotation[0] = 0;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }

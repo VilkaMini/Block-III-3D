534506130d47c50ced1beb2a14165c0e1d7b0fde
kvm1l-dg <kamil.slomnicki1@gmail.com>
2023-03-14 14:25:24 +0100

    Colission mask

---
+++ Zoo Project/Assets/Scripts/AnimalInteraction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalInteraction : MonoBehaviour {
+
+    private void OnCollisionStay(Collision collision) {
+        //Check if player is next to an animal 
+       if (collision.gameObject.tag == "Player") {
+            //Check if button is pressed 
+            if (Input.GetKey(KeyCode.E)) {
+                Debug.Log("clicked");
+
+            }
+        }
+
+    }
+
+}
+

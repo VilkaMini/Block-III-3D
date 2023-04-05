7f3410567fd4edfcc8d6c2895095ededabd95e55
Vilius <alaunisvilius@gmail.com>
2023-04-05 14:24:17 +0200

    Added mroe texr

--- Zoo Project/Assets/Scriptsv2/Storage.cs
+++ Zoo Project/Assets/Scriptsv2/Storage.cs
@@ -12,8 +12,8 @@ public static class Storage
     // Information on animals
     public static Dictionary<string, Dictionary<string, string[]>> animalInfo = new Dictionary<string, Dictionary<string, string[]>>()
     {
-        {"Rhino", new Dictionary<string, string[]>(){ {"Head", new string[] {"Head", "par 1", "par 2"} },
-                                                      {"Ears", new string[] {"Ears", "", ""}},
+        {"Rhino", new Dictionary<string, string[]>(){ {"Head", new string[] {"Head", "Rhinos are capable of producing 10 different types of vocalizations.", "Furthermore rhinos make use of grunts as a form of greeting and produce a <color=#FFFF00> “mmwonk” </color> sound to indicate a state of happiness." } },
+                                                      {"Ears", new string[] {"Ears", "", ""}},  
                                                       {"Body", new string[] {"Body", "", ""}},
                                                       {"Front Legs", new string[] {"Front Legs", "", ""}},
                                                       {"Back Legs", new string[] {"Back Legs", "", ""}},
